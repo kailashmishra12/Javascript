3b5e0a3e096a50e94b67210898604188023deb25
kailashmishra12 <140958362+kailashmishra12@users.noreply.github.com>
2024-08-22 15:45:54 +0000

    Datatype

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,18 @@
+
+const accountId=123;
+var id="kailash";
+let str="kailash mishra"
+console.table([accountId,str,id]);
+
+/* prefer not to use var
+because of issue in block scope and functional scope */ 
+
+//number=>2 to the power 53
+//bigint
+//string=>""
+//boolean =>trye/false
+//null=>empty
+//undefined=>we cannot assign value at that point
+//symbols=>unique
+
+//console.log(typeof(null)); object
